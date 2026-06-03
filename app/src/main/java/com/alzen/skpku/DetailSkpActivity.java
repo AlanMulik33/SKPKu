d66c1ab959f8392bf2e67922daad4a27c8fc2d4c
wisatwan <wisatwan06@gmail.com>
2026-06-03 15:57:42 +0800

    buat iddan logika main

---
+++ app/src/main/java/com/alzen/skpku/DetailSkpActivity.java
@@ -0,0 +1,17 @@
+package com.alzen.skpku;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ * File sementara untuk halaman Detail SKP.
+ * Nanti di langkah berikutnya kode ini akan diganti menjadi detail lengkap.
+ */
+public class DetailSkpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_skp);
+    }
+}
---
+++ app/src/main/java/com/alzen/skpku/FormSkpActivity.java
@@ -0,0 +1,15 @@
+package com.alzen.skpku;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+/*
+ * File sementara untuk halaman Form SKP.
+ * Nanti di langkah berikutnya kode ini akan diganti menjadi form input/edit lengkap.
+ */
+public class FormSkpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_skp);
+    }
+}
